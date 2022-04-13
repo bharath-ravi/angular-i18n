eac08a24d26d4438f93c9ac68c5ef7cd2b2bfc88
bharath-ravi <bharath.dsi05@gmail.com>
2022-04-13 11:15:35 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { TranslateService } from './translate.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  namee = 'Angular ';
+export class AppComponent {
+  namee = 'Angular';
+  version = 13;
+  userName = 'user';
   constructor(public translate: TranslateService) {
-    this.translate.use('en');
+    // this.translate.use('en');
+  }
+
+  changeLanguage(LangCode: any) {
+    this.translate.use(LangCode);
   }
 }
--- src/app/translate.pipe.ts
+++ src/app/translate.pipe.ts
@@ -9,26 +9,33 @@ export class TranslatePipe implements PipeTransform {
   constructor(private translate: TranslateService) {}
 
   transform(key: any, ...args: any[]): any {
-    console.log(args.length, 'args.length');
+
     if (args.length > 0) {
-      console.log(this.removeInterPoll(this.translate.data[key], args));
-      let data = this.removeInterPoll(this.translate.data[key], args);
+      console.log(this.removeInterPoll(this.translate.data[key], args),'after');
       return this.removeInterPoll(this.translate.data[key], args) || key;
     } else {
+      debugger
       return this.translate.data[key] || key;
     }
-    // console.log(data , 'hi');
   }
-  removeInterPoll(keyValue: any, args: any): any {
+
+
+  public removeInterPoll(keyValue: any, args: any): any {
+    let replacedData:any;
     args.forEach((ele, index) => {
+      let value = index > 0? replacedData: keyValue;
       Object.keys(ele).forEach((objKey) => {
         let keyName = `{{${objKey}}}`;
-        let replacedData = keyValue.replace(new RegExp(keyName, 'g'), '');
+        replacedData = value.replace(
+          new RegExp(keyName, 'g'),
+          ele[objKey]
+        );
         // console.log(keyName,ele[objKey],keyValue,'ele');
-        console.log(replacedData);
-        return replacedData;
+        console.log(replacedData, 'replacedData');
+
       });
     });
+    return replacedData;
     // console.log(keyValue.replace(new RegExp(oldWord, "g"), " abc");)
     // console.log('args',keyValue,args)
   }
